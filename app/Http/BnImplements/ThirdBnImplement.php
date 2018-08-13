41ca49ad9d55cb6dfe8ebdc41c2e3be8557661d7
NESTOR MORENO <nmoreno@overcom.net>
2018-08-13 10:11:49 -0400

    location exist

--- app/Http/BnImplements/ThirdBnImplement.php
+++ app/Http/BnImplements/ThirdBnImplement.php
@@ -182,7 +182,7 @@ class ThirdBnImplement
 				:manufacturer,
 				:po_price_list_id,
 				:language_id,
-                :greeting_id
+                :greeting_id,
                 :id
 			)', [
 					'org_id' => $org_id,
@@ -216,7 +216,23 @@ class ThirdBnImplement
 				]
 			);
 
-		$location_insert = $this->location_implement
+		if ( $third_location['id'] == 0 ) {
+			$location_insert = $this->location_implement
+				->insertLocation(
+					$conection, $third_location['address_1'],
+					$third_location['address_2'], $third_location['address_3'],
+					$third_location['address_4'], $third_location['city_id'],
+					$third_location['city_name'], $third_location['postal'],
+					$third_location['postal_add'], $third_location['state_id'],
+					$third_location['state_name'], $third_location['country_id'],
+					$third_location['comments']
+				);
+			$location_id = $location_insert;
+		} else {
+			$location_id = $third_location['id'];
+		}
+
+		/* $location_insert = $this->location_implement
 			->insertLocation(
 				$conection, $third_location['address_1'],
 				$third_location['address_2'], $third_location['address_3'],
@@ -225,17 +241,17 @@ class ThirdBnImplement
 				$third_location['postal_add'], $third_location['state_id'],
 				$third_location['state_name'], $third_location['country_id'],
 				$third_location['comments']
-			);
+			); */
 
 		$branch_office_insert = $this->insertBranchOffice(
-			$conection, $lid[0]->LID, $location_insert,
+			$conection, $lid[0]->LID, $location_id,
 			$name, 1, 1, 1, 1, $branch_office['phone'],
 			$branch_office['phone_2'], '', ''
 		);
 
 		return [
 			'third' => $lid[0]->LID,
-			'location' => $location_insert,
+			'location' => $location_id,
 			'branch_office' => $branch_office_insert
 		];
 	}
