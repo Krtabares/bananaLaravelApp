f965d0931e6832c0cec47ff20e61aee4823cfd4b
kerby tabares <krtabares@gmail.com>
2018-08-17 15:23:03 -0400

    commit

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,6 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
-// header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With, app, token, user');
